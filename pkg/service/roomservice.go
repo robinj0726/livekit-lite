43e01003bdea4a898fec222fa85ea5de1e11a2a2
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-08-05 17:45:14 +0800

    impl livekit.RoomService interface

---
+++ pkg/service/roomservice.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"context"
+
+	"github.com/livekit/protocol/livekit"
+)
+
+type RoomService struct{}
+
+func NewRoomService() (svc *RoomService, err error) {
+	svc = &RoomService{}
+
+	return
+}
+
+func (s *RoomService) CreateRoom(ctx context.Context, req *livekit.CreateRoomRequest) (rm *livekit.Room, err error) {
+	return
+}
+
+func (s *RoomService) ListRooms(ctx context.Context, req *livekit.ListRoomsRequest) (res *livekit.ListRoomsResponse, err error) {
+	return
+}
+
+func (s *RoomService) DeleteRoom(ctx context.Context, req *livekit.DeleteRoomRequest) (*livekit.DeleteRoomResponse, error) {
+	return &livekit.DeleteRoomResponse{}, nil
+}
+
+func (s *RoomService) ListParticipants(ctx context.Context, req *livekit.ListParticipantsRequest) (res *livekit.ListParticipantsResponse, err error) {
+	return
+}
+
+func (s *RoomService) GetParticipant(ctx context.Context, req *livekit.RoomParticipantIdentity) (res *livekit.ParticipantInfo, err error) {
+	return
+}
+
+func (s *RoomService) RemoveParticipant(ctx context.Context, req *livekit.RoomParticipantIdentity) (res *livekit.RemoveParticipantResponse, err error) {
+	return
+}
+
+func (s *RoomService) MutePublishedTrack(ctx context.Context, req *livekit.MuteRoomTrackRequest) (res *livekit.MuteRoomTrackResponse, err error) {
+	return
+}
+
+func (s *RoomService) UpdateParticipant(ctx context.Context, req *livekit.UpdateParticipantRequest) (*livekit.ParticipantInfo, error) {
+	return &livekit.ParticipantInfo{}, nil
+}
+
+func (s *RoomService) UpdateSubscriptions(ctx context.Context, req *livekit.UpdateSubscriptionsRequest) (*livekit.UpdateSubscriptionsResponse, error) {
+	return &livekit.UpdateSubscriptionsResponse{}, nil
+}
+
+func (s *RoomService) SendData(ctx context.Context, req *livekit.SendDataRequest) (*livekit.SendDataResponse, error) {
+	return &livekit.SendDataResponse{}, nil
+}
+
+func (s *RoomService) UpdateRoomMetadata(ctx context.Context, req *livekit.UpdateRoomMetadataRequest) (*livekit.Room, error) {
+	return &livekit.Room{}, nil
+}
